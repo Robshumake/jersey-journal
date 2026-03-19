1d3cd930b31b5f921aaa73ba6a2c44f492095d9f
Rob Shumake <robshumake@gmail.com>
2026-03-19 09:25:51 -0500

    Fix CSS animation + missing deps

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -33,6 +33,15 @@ const config: Config = {
         '4xl': ['36px', { lineHeight: '40px' }],
         '5xl': ['48px', { lineHeight: '48px' }],
       },
+      animation: {
+        marquee: 'marquee 30s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
+      },
     },
   },
   plugins: [],
